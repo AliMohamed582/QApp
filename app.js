852f18c15b1aa6613eab92d334cac40cd73a8e0f
Ali <alimohamedaliebis10a@gmail.com>
2025-12-09 19:30:07 +0200

    Ali: Express server setup

--- app.js
+++ app.js
@@ -1,50 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+  const express = require('express');
+    const app = express();
+    const port = 3000;
 
-const app = express();
-const port = 3000;
+    app.use(express.json());
 
-// Load environment variables
-dotenv.config();
-
-// Middleware
-app.use(express.json());
-
-const uri = process.env.MONGODB_URI;
-if (!uri) {
-  console.error("❌ MONGODB_URI missing in .env");
-  process.exit(1);
-}
-
-mongoose.connect(uri)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => {
-    console.error("❌ Connection Error:", err.message);
-    process.exit(1);
+  app.get('/', (req, res) => {
+    res.send('QApp Backend Running');
   });
-
-
-const userRoutes = require('./routes/userRoutes');
-const queueRoutes = require('./routes/queueRoutes');
-
-const authRoutes = require('./routes/authRoutes');
-const branchRoutes = require('./routes/branchRoutes');
-const serviceRequestRoutes = require('./routes/serviceRequestRoutes');
-
-app.use('/api', userRoutes);
-app.use('/api', queueRoutes);
-
-app.use('/api', authRoutes);
-app.use('/api', branchRoutes);
-app.use('/api', serviceRequestRoutes);
-
-
-app.get('/', (req, res) => {
-  res.send('QApp Backend Running');
-});
-
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  
+ app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
